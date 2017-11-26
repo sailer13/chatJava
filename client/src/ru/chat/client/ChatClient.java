a92204680d2f83720d0d7a593438fc10c8f5c43b
Shishkov Ivan <cellentano123.1999@gmail.com>
2017-11-26 12:20:56 +0300

    15

--- client/src/ru/chat/client/ChatClient.java
+++ client/src/ru/chat/client/ChatClient.java
@@ -5,21 +5,80 @@ import ru.chat.network.TCPConnectionListener;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import java.io.IOException;
 
 /**
  * Created by User on 21.11.2017.
  */
 public class ChatClient extends  JFrame implements ActionListener, TCPConnectionListener{
-
-    private ChatNetwork network;
+    private ListenerFactory factory = new ListenerFactory();
 
     private static  final String IP_ADRES = "192.168.1.5";
     private static  final  int PORT = 8187;
     private static final int WIDTH = 400;
     private static final int HEIGHT = 400;
+
+    private  final JTextArea log = new JTextArea();
+    private  final JTextField fieldName = new JTextField("1234");
+    private  final JTextField fieldInput = new JTextField();
+
+    private ChatNetwork network;
+
+    private ChatClient() {
+        super("ChatClient");
+        addWindowListener(factory.getWindowL());
+
+        setSize(WIDTH,HEIGHT);
+        setLocationRelativeTo(null);
+        setAlwaysOnTop(true);
+        log.setEditable(false); //РЅРёР·СЏ РїРёСЃР°С‚СЊ С‚Р°Рј РґРµ РІС‹РІРѕРґРёС‚ СЃРѕРѕР±Р¶РµРЅРёСЏ
+        log.setLineWrap(true);
+
+        add(log, BorderLayout.CENTER);
+
+
+        add(fieldInput, BorderLayout.SOUTH);
+        add(fieldName, BorderLayout.NORTH);
+
+        fieldInput.addActionListener(factory.getFieldInput());
+
+
+        setVisible(true);
+
+        try {
+            network = new ChatNetwork(this, IP_ADRES, PORT);
+        } catch (IOException e) {
+            printMessage("Cpnnection exception "+e);
+        }
+
+    }
+
+    class ListenerFactory {
+        public ActionListener getFieldInput() {
+            return new ActionListener() {
+                public   void actionPerformed(ActionEvent e) {
+
+                    String msg = fieldInput.getText();
+                    if(msg.equals(""))return;
+                    fieldInput.setText(null);
+                    network.sendString(fieldName.getText()+": "+msg);
+                }
+            };
+        }
+        public WindowListener getWindowL() {
+            return new WindowL();
+        }
+        class WindowL extends WindowAdapter {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        }
+    }
+
+
+
+
 
     public  static void main(String[]args){
         SwingUtilities.invokeLater(new Runnable() {
@@ -30,34 +89,7 @@ public class ChatClient extends  JFrame implements ActionListener, TCPConnection
         });
     }
 
-    private  final JTextArea log = new JTextArea();
-    private  final JTextField fieldName = new JTextField("1234");
-    private  final JTextField fieldInput = new JTextField();
 
-    private ChatClient() {
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setSize(WIDTH,HEIGHT);
-        setLocationRelativeTo(null);
-        setAlwaysOnTop(true);
-        log.setEditable(false);
-        log.setLineWrap(true);
-
-        fieldInput.addActionListener(this);
-
-        add(log, BorderLayout.CENTER);
-        add(fieldInput, BorderLayout.SOUTH);
-        add(fieldName, BorderLayout.NORTH);
-
-        setVisible(true);
-
-        try {
-            network = new ChatNetwork(this, IP_ADRES, PORT);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
